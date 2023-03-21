e8f47846801a065750a2399d79f0342c844a3416
Mehul Todi <75692643+kid-116@users.noreply.github.com>
2023-03-21 12:50:23 +0530

    UI fixes

--- greentrust/src/components/Logo.js
+++ greentrust/src/components/Logo.js
@@ -7,7 +7,7 @@ export default function Logo() {
         <div className="flex flex-row gap-[12px]">
             <img src="/logo.png" width={64} height={64} />
             <div>
-                <p className="text-center font-poppins">
+                <p className="text-center font-poppins hidden sm:block">
                     <span className="text-primary font-extrabold text-3xl block">Green</span>
                     <span className="text-gray block uppercase font-medium text-xl tracking-widest">Trust</span>
                 </p>
